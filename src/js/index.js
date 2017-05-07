d5158d0bfd562d44052a6f048c711e38fda76312
narin <narinbsru@gmail.com>
2017-05-07 12:24:52 +0700

    add node file

--- src/js/index.js
+++ src/js/index.js
@@ -18,37 +18,28 @@ ReactDOM.render( <MainApp/> ,document.getElementById('mainapp') );
 (function () {
     var ctx = document.getElementById('myChart');
     new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                label: 'Scatter Dataset',
+                data: [{
+                        x: -10,
+                        y: 0
+                    }, {
+                        x: 0,
+                        y: 10
+                    }, {
+                        x: 10,
+                        y: 5
+                }]
             }]
         },
         options: {
+            responsive: true,
             scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
                 }]
             }
         }
